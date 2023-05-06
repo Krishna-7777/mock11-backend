b812e105a094ace01ea39964c037a06bd549a6e8
Krishna Chhabra <112754677+Krishna-7777@users.noreply.github.com>
2023-05-06 14:48:25 +0530

    Add files via upload

--- models/notice.model.js
+++ models/notice.model.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose")
 
 const schema=mongoose.Schema({
-    text:String
+    name:String,
+    title:String,
+    text:String,
+    date:{type:Date,default:Date.now()}
 })
 
 const NoticeModel=mongoose.model('notice',schema);
